20da74fe7dd03c46352fe16c6e52c93528cea030
Aaron Juarez <Aaron Juarez@DESKTOP-DKTIVHI>
2025-03-08 17:46:10 -0600

    Agregar archivos de proyecto.

---
+++ Practica.CodeFirst/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Practica.CodeFirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
