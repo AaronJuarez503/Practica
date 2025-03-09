6fd26e59fa5f00574e1f12ab09505a22c0037060
Aaron Juarez <Aaron Juarez@DESKTOP-DKTIVHI>
2025-03-08 20:36:30 -0600

    Crud completo del CodeFirst

---
+++ Practica.CodeFirst/Models/ESFEDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Practica.CodeFirst.Models
+{
+    public class ESFEDbContext: DbContext
+    {
+        public ESFEDbContext(DbContextOptions<ESFEDbContext> options) : base(options) { }
+
+        public DbSet<Empleado> Empleados { get; set; }
+        public DbSet<Alumno> Alumnos { get; set; }
+
+    }
+}
